e35471b3fbdc1c7c4c7ee66646754c553c1f1c14
ecomeel <ecomeel@gmail.com>
2023-06-24 09:15:42 +0300

    done app

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const titleNode = document.getElementById('title');
+const subtitleNode = document.getElementById('subtitle');
+const getIdeaBtnNode = document.getElementById('getIdeaBtn');
+
+const setNewActivity = () => {
+    fetch("http://www.boredapi.com/api/activity/")
+    .then(response => response.json())
+    .then(res => {
+        subtitleNode.innerText = res.activity;
+    })
+}
+
+const changeBackground = () => {
+    document.body.className = 'green-gradient-back';
+}
+
+const changeTitle = () => {
+    titleNode.innerText = 'Ура, теперь не скучно 🔥'
+}
+
+const getIdeaHandler = () => {
+    changeTitle();
+    changeBackground();
+    setNewActivity();
+}
+
+getIdeaBtnNode.addEventListener('click', getIdeaHandler);
+
